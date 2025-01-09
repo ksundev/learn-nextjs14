71801e44f392d2203f3b41a4aa7d4806a5659112
ksundev <kshk08@hanyang.ac.kr>
2025-01-10 04:23:57 +0900

    refactor: Use Navigation as Layout

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -2,10 +2,12 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import React from "react";
 
 export default function Navigation() {
   const path = usePathname();
   console.log(path);
+  const [count, setCount] = React.useState(0);
   return (
     <nav>
       <ul>
@@ -16,6 +18,7 @@ export default function Navigation() {
           <Link href="/about-us">About Us</Link>{" "}
           {path === "/about-us" ? "📌" : ""}
         </li>
+        <li><button onClick={() => setCount(c => c + 1)}>{count}</button></li>
       </ul>
     </nav>
   );
