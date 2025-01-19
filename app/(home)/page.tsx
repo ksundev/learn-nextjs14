1d3025cb107802b4c646ca796bf11cea64b94128
ksundev <kshk08@hanyang.ac.kr>
2025-01-19 20:03:13 +0900

    feat: Add home page with classic fetch API for movies

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,11 +1,26 @@
-export const metadata = {
-  title: "Home",
-};
+"use client";
+
+import { useEffect, useState } from "react";
+
+const API_URL = "https://nomad-movies.nomadcoders.workers.dev";
 
 export default function Page() {
+  const [movies, setMovies] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+  const getMovies = async () => {
+    const response = await fetch(`${API_URL}/movies`);
+    const json = await response.json();
+    setMovies(json);
+    setIsLoading(false);
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
+
   return (
     <div>
       <h1>Home</h1>
+      {isLoading ? `Loading...` :JSON.stringify(movies)}
     </div>
   );
 }
