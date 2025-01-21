0f1b3544a8f87243020a5984dfa3cbf766b8aacf
ksundev <kshk08@hanyang.ac.kr>
2025-01-22 06:56:12 +0900

    feat: Apply css into pages

--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -1,15 +1,18 @@
 import { API_URL } from "../app/(home)/page";
 
 async function getVideo(id: string) {
-    console.log(`Fetching videos: ${Date.now()}`);
-    await new Promise((resolve) => setTimeout(resolve, 3000));
-    const response = await fetch(`${API_URL}/movies/${id}/videos`);
-    return await response.json();
+  console.log(`Fetching videos: ${Date.now()}`);
+  // await new Promise((resolve) => setTimeout(resolve, 3000));
+  // throw new Error("something went wrong...")
+  const response = await fetch(`${API_URL}/movies/${id}/videos`);
+  return await response.json();
 }
 
-export default async function MovieVideos({id}: {id: string}) {
-    const videos = await getVideo(id);
-    return <div>
-        <h6>{JSON.stringify(videos)}</h6>
+export default async function MovieVideos({ id }: { id: string }) {
+  const videos = await getVideo(id);
+  return (
+    <div>
+      <h6>{JSON.stringify(videos)}</h6>
     </div>
+  );
 }
--- components/navigation.tsx
+++ components/navigation.tsx
@@ -2,14 +2,13 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import React from "react";
+import styles from "../styles/navigation.module.css";
 
 export default function Navigation() {
   const path = usePathname();
   // console.log(path);
-  const [count, setCount] = React.useState(0);
   return (
-    <nav>
+    <nav className={styles.nav}>
       <ul>
         <li>
           <Link href="/">Home</Link> {path === "/" ? "📌" : ""}
@@ -18,7 +17,6 @@ export default function Navigation() {
           <Link href="/about-us">About Us</Link>{" "}
           {path === "/about-us" ? "📌" : ""}
         </li>
-        <li><button onClick={() => setCount(c => c + 1)}>{count}</button></li>
       </ul>
     </nav>
   );
